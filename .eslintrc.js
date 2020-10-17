efb4cd14c8164cf5789753f0c9f9756570dcc2a0
夕舞八弦 <1677759063@qq.com>
2020-10-17 16:00:35 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      jest: true,
+    },
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    browser: true,
+    jasmine: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
